3189a05b97735ccd4640274187218041161cc120
Vulture <thomas.merel.44@gmail.com>
2020-07-08 20:56:37 +0200

    add http-server

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 require('dotenv').config({path: `.env`});
 
+// Load the http module to create an http server.
+const http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+const server = http.createServer((request, response) => {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("START HTTP SERVER\n");
+});
+
+// Last, but not least, listen on port 8080
+// The environment variable PORT is automatically defined and equals to 8080
+server.listen(process.env["PORT"], '0.0.0.0');
+
 //Set
 const Discord = require('discord.js');
 const CronJob = require('cron').CronJob;
